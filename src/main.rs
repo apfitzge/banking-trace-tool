6e950d96d630566f6565501e5f453648dc2ebcd5
Andrew Fitzgerald <apfitzge@gmail.com>
2023-10-25 19:09:18 +0800

    Add setup (#10)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use clap::Parser;
+use {crate::cli::Cli, clap::Parser, setup::get_event_file_paths};
 
 mod cli;
 mod process;
+mod setup;
 
 fn main() {
-    let args = cli::Cli::parse();
-    println!("Args: {args:?}");
+    let Cli { path, mode } = Cli::parse();
+    let _event_file_paths = get_event_file_paths(path);
+
+    match mode {}
 }
